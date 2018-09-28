2cbeb3a804fe77a122e1a9b26ea5f663bae73b7a
jbdlivejp <j-b-d@live.jp>
2018-09-28 15:14:25 +0900

    2018

--- amoti-admin/src/com/internousdev/amoti/action/AdminUserEditAction.java
+++
@@ -1,9 +0,0 @@
-package com.internousdev.amoti.action;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class AdminUserEditAction extends ActionSupport{
-		public String execute(){
-			return SUCCESS;
-		}
-}
