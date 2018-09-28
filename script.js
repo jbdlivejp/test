451fe35effa99f4440069e8f22c30635670f8657
jbdlivejp <j-b-d@live.jp>
2018-09-28 14:52:08 +0900

    2018

--- script.js
+++
@@ -1,6 +0,0 @@
-function totalPrice(otona, kodomo){
-	return otona*500 + kodomo*200;
-}
-	document.write(totalPrice(2,4) + "<br>");
-	document.write(totalPrice(1,5) + "<br>");
-	document.write(totalPrice(3,7) + "<br>");
